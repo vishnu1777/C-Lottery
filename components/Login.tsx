27e879645e21ef61b2355c8ab17ee43eb15db198
Vishnu <vappu5429@gmail.com>
2023-04-03 22:05:23 +0530

    Practiced Login Page

--- components/Login.tsx
+++ components/Login.tsx
@@ -5,20 +5,21 @@ type Props = {}
 function Login({}: Props) {
   const connnectWithMetamask = useMetamask()
   return (
-    <div className='bg-[#091B18] min-h-screen flex flex-col items-center justify-center text-center'>
-      <div className='flex flex-col items-center mb-10'>
-        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQq8N3dSWad5RKfCiLsW70dLYbs88dwOuRUgg&usqp=CAU" alt="logo"
-        className='rounded-full h-56 w-56 mb-10'
-        />
-        <h1 className='text-6xl text-white font-bold '>C-Lottery Draw</h1>
-        <h2 className='text-white py-2 '>Get Started By Logging In With Your Metamask</h2>
-        <button
-        onClick={connnectWithMetamask} 
-        className='bg-white px-8 py-5 mt-10 rounded-lg shadow-lg font-bold'>
-          Log In With Metamask
-        </button>
+      <div className='bg-[#091B18] h-screen flex justify-center items-center'>
+          <div className='flex flex-col items-center mb-10'>
+              <img
+              className='h-56 mb-10 w-56 rounded-full'
+              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQq8N3dSWad5RKfCiLsW70dLYbs88dwOuRUgg&usqp=CAU" alt="MyLogo" />
+              <h1 className='text-6xl font-bold text-white '>C-Lottery Draw</h1>
+              <p className='text-white text-sm py-2'>Get Started By Logging In With Your Metamask</p>
+              <button
+              onClick={connnectWithMetamask}
+              className='bg-white px-8 py-5 rounded-lg mt-10 font-bold'
+              >
+                Login With Metamask
+              </button>
+          </div>
       </div>
-    </div>
   )
 }
 
